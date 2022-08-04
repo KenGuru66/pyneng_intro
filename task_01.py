c054feff4f466e51eb15b3dc9d66c8f6a8954fc9
pyneng <pyneng.course@gmail.com>
2022-08-04 13:36:44 +0000

    tasks 1-3

--- task_01.py
+++ task_01.py
@@ -1,20 +1,16 @@
 """
-Задание 1.1
-
 Если запустить код задания, будет такой вывод:
-$ python task_1_1.py
-line1
-line2
-line3
+$ python task_01.py
+Python is a high-level, interpreted, general-purpose programming language.
 
 Надо преобразовать строку start_data таким образом, чтобы на экран была выведена
-такая строка
-$ python task_1_1.py
-line1 line2 line3
+такая строка (заменить Python на Ruby):
+$ python task_01.py
+Ruby is a high-level, interpreted, general-purpose programming language.
 
 При этом нельзя менять строку start_data вручную, то есть нельзя менять исходную строку
 start_data.
 """
-start_data = "line1\nline2\nline3"
+start_data = "Python is a high-level, interpreted, general-purpose programming language."
 print(start_data)
 
